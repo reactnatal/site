f61d4837973c697277bbe0858643d622f4ce016a
annyconfessor <annyconfessor.ectufrn@gmail.com>
2019-10-10 09:58:19 -0300

    Lista de espera

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
           <ul>
             <li>Local: <strong><a href="http://jerimumhacker.space" target="_blank" rel="noopener noreferrer">Jerimum Hackerspace</a></strong>. (<a href="https://bit.ly/2n1zps0" target="_blank" rel="noopener noreferrer">Ver mapa</a>)</li>
             <li>Data: <strong>26/10 das 14h às 18h</strong>.</li>
-            <li>Inscrições: <strong><a href="https://bit.ly/2pmQLAh" target="_blank" rel="noopener noreferrer">https://bit.ly/2pmQLAh</a></strong>. (Vagas limitadas)</li>
+            <li>Lista de Espera: <strong><a href="https://forms.gle/QQhYXVSpY53NG3qE7" target="_blank" rel="noopener noreferrer">https://bit.ly/2pmQLAh</a></strong>. (Vagas limitadas)</li>
           </ul>
           <p>* Por favor, se possivel trazer notebook para participar do mini curso.</p>
           <p>** É novo por aqui? Preencha <a href="https://docs.google.com/forms/d/e/1FAIpQLSfoa7A91QVldfTJJz7o6chDN4MxMADtjLuSJ5TlipqI6swL4A/viewform" target="_blank" rel="noopener noreferrer">nossa pesquisa</a> para conhecermos melhor o perfil da comunidade.</p>
